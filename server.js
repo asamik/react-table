12e7d3e60a6079d879e13ae083a659687797859e
Asami Kawamoto <asami236@gmail.com>
2015-12-04 23:08:07 -0800

    Edit packageJSON

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-var express = require("express");
+"use strict";
 
-var app = express();
 var PORT = process.env.PORT || 3000
+
+var express = require("express");
+var app = express();
 
 app.use(express.static('public'));
 
-app.listen(PORT);
+app.listen(PORT, function(){
+  console.log('Listening on port ', PORT);
+});
